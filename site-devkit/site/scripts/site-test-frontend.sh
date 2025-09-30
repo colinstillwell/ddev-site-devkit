729265f19aae5c3cb15b38af1f8187db9ed747eb
Colin Stillwell <cstillwell@caci.co.uk>
2025-09-30 12:13:55 +0100

    [25] Improve the messaging for all example scripts, directing to the README

--- site-devkit/site/scripts/site-test-frontend.sh
+++ site-devkit/site/scripts/site-test-frontend.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 #ddev-generated
-## Example provided by https://github.com/colinstillwell/ddev-site-devkit
 
 # Exit on error; treat unset variables as errors; fail pipelines if any command fails
 set -euo pipefail
 
-echo "This is an example for 'ddev site-test-frontend'."
+cat <<'EXAMPLE'
+This script has not been customised for your project yet.
+
+For guidance on how to adapt it, see "Customising the generated scripts" in the add-on documentation:
+https://github.com/colinstillwell/ddev-site-devkit#customising-the-generated-scripts
+EXAMPLE
